1f6fcc721d98e1e767b30e8015cb51dd020b08fe
Xi Huang <xhuang@ucdavis.edu>
2023-01-01 02:07:52 -0800

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by 黄熙 on 1/1/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
