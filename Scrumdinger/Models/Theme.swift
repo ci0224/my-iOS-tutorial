a85e7e9ce9a7e7f5b8a299831ceb53e263397643
Xi Huang <xhuang@ucdavis.edu>
2023-01-01 21:59:14 -0800

    saved

---
+++ Scrumdinger/Models/Theme.swift
@@ -0,0 +1,42 @@
+//
+//  Theme.swift
+//  Scrumdinger
+//
+//  Created by 黄熙 on 1/1/23.
+//
+
+import Foundation
+
+import SwiftUI
+
+enum Theme: String {
+
+    case bubblegum
+    case buttercup
+    case indigo
+    case lavender
+    case magenta
+    case navy
+    case orange
+    case oxblood
+    case periwinkle
+    case poppy
+    case purple
+    case seafoam
+    case sky
+    case tan
+    case teal
+    case yellow
+    
+    var accentColor: Color {
+        switch self {
+        case .bubblegum, .buttercup, .lavender, .orange, .periwinkle, .poppy, .seafoam, .sky, .tan, .teal, .yellow: return .black
+        case .indigo, .magenta, .navy, .oxblood, .purple: return .white
+        }
+    }
+    
+    var mainColor: Color {
+            Color(rawValue)
+        }
+}
+
---
+++ Scrumdinger/TrailingIconLabelStyle.swift
@@ -0,0 +1,25 @@
+//
+//  TrailingIconLabelStyle.swift
+//  Scrumdinger
+//
+//  Created by 黄熙 on 1/1/23.
+//
+
+import Foundation
+
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
