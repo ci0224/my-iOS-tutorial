b9d1df6b5892ac1a7b1fc511f59bac4bd2225314
Xi Huang <xhuang@ucdavis.edu>
2023-01-05 16:19:18 -0800

    done managing-state-and-life-cycle

--- Scrumdinger/MeetingHeaderView.swift
+++ Scrumdinger/MeetingHeaderView.swift
@@ -29,6 +29,7 @@ struct MeetingHeaderView: View {
     var body: some View {
         VStack {
             ProgressView(value:progress)
+                .progressViewStyle(ScrumProgressViewStyle(theme: theme))
             HStack {
                 VStack(alignment: .leading) {
                     Text("Seconds Elapsed")
@@ -40,12 +41,14 @@ struct MeetingHeaderView: View {
                     Text("Seconds Remaining")
                         .font(.caption)
                     Label("\(secondsRemaining)", systemImage: "hourglass.tophalf.fill")
+                        .labelStyle(.trailingIcon)
                 }
             }
         }
         .accessibilityElement(children: .ignore)
         .accessibilityLabel("Time remaining")
         .accessibilityValue("\(minutesRemaining) minutes")
+        .padding([.top, .horizontal])
     }
 }
 
